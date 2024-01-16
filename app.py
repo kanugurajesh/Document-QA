4249d492edee4fd26e127a7153f8b311854bc1ea
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-16 21:22:22 +0530

    Adding all the streamlit code

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("Ask Questions from PDF Files")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
